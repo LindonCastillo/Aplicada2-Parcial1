36f964b0431d2240b70d319ae80ac637c0315f2d
Lindon Emil Castillo Burgos <lindon_castillo@ucne.edu.do>
2020-02-04 22:14:04 -0400

    DbContext hecho y string connection

--- Aplicada2_Parcial1/Aplicada2_Parcial1/Data/Contexto.cs
+++ Aplicada2_Parcial1/Aplicada2_Parcial1/Data/Contexto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,10 @@ namespace Aplicada2_Parcial1.Data
 {
     public class Contexto : DbContext
     {
-
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Server = .\SqlExpress; Database= Parcial1_Db; Trusted_Connection = True; ");
+        }
     }
 }
